9f1de0cea4805ac829146bf35376267649e8bd32
ssboui <ssboui>
2022-06-24 11:06:09 +0100

    initial commit

---
+++ src/test/java/com/fininfosolutions/fwk/fininfosolutionsregistry/FininfosolutionsRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fininfosolutions.fwk.fininfosolutionsregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FininfosolutionsRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
